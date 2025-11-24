0b735ccdd776658b5a7996fc85b25b1387488044
jsnmos <dbwltn0115@jj.ac.kr>
2025-11-24 22:56:39 +0900

    Update script.js

--- res/script.js
+++ res/script.js
@@ -34,20 +34,26 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-document.getElementById("estimateBtn").addEventListener("click", function(e) {
-  e.preventDefault(); // 기존 링크 기본 동작 막기
+document.addEventListener("DOMContentLoaded", () => {
 
+  // --- 팝업 열기 기능 ---
+  const popupButtons = document.querySelectorAll(".openPopup");
   // 팝업 창 옵션
   const width = 700;
   const height = 900;
   const left = (window.screen.width / 2) - (width / 2);
   const top = (window.screen.height / 2) - (height / 2);
 
-  // 새 창으로 열기
-  window.open(
-    this.href,
-    "견적문의팝업", 
-    `width=${width},height=${height},top=${top},left=${left},resizable=yes,scrollbars=yes`
-  );
+  popupButtons.forEach(btn => {
+    btn.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      window.open(
+        "https://oktarnd.kr/popup.html",
+        "estimatePopup",
+        "width=${width},height=${height},top=${top},left=${left}"
+      );
+    });
+  });
 
 });
